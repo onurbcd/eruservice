68b1acc862d91b0dadca4f8cc5863223f5d51ef5
Bruno Domingues <onurbcd@gmail.com>
2022-06-10 23:59:57 -0300

    remove name unique constraint from budget

--- src/main/java/com/onurbcd/eruservice/persistency/entity/Budget.java
+++ src/main/java/com/onurbcd/eruservice/persistency/entity/Budget.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.AttributeOverride;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -16,6 +17,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Getter
 @Setter
+@AttributeOverride(name = "name", column = @Column())
 public class Budget extends Prime {
 
     @NotNull

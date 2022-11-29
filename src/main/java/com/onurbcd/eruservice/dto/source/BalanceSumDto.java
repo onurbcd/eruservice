1ddb285591bd8002d2fa2831a872d14af75996fe
Bruno Domingues <onurbcd@gmail.com>
2022-11-28 22:15:44 -0300

    refactor balance sum all args constructor

--- src/main/java/com/onurbcd/eruservice/dto/source/BalanceSumDto.java
+++ src/main/java/com/onurbcd/eruservice/dto/source/BalanceSumDto.java
@@ -1,14 +1,13 @@
 package com.onurbcd.eruservice.dto.source;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import java.math.BigDecimal;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class BalanceSumDto {
@@ -16,4 +15,9 @@ public class BalanceSumDto {
     private BigDecimal partial;
 
     private BigDecimal total;
+
+    public BalanceSumDto(@Nullable BigDecimal partial, @Nullable BigDecimal total) {
+        this.partial = partial != null ? partial : BigDecimal.ZERO;
+        this.total = total != null ? total : BigDecimal.ZERO;
+    }
 }
